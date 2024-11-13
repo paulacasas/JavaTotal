4db40ab753c998365204edf33d9e7feddea4c730
PaulaCf <paulacasas2022@gmail.com>
2024-11-12 20:36:04 -0500

    Ejercicio Poligono demo

---
+++ Poligono/Poligono.java
@@ -0,0 +1,14 @@
+package Poligono;
+
+public abstract class Poligono {
+    protected int lados;
+
+    public Poligono(int lados) {
+        this.lados = lados;
+    }
+
+    public int getLados() {
+        return lados;
+    }
+
+}
---
+++ Poligono/Rectangulo.java
@@ -0,0 +1,15 @@
+package Poligono;
+
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public double calcularArea() {
+        return lado1 * lado2;
+    }
+}
