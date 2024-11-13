4c49f3c4c87a39d2e831fd18f77f2cbe4bdfee5e
PaulaCf <paulacasas2022@gmail.com>
2024-11-12 19:40:15 -0500

    Clase principal Funcionando

--- Basico/src/POO/Aprendiendo/Herencia/MetodosAbstactos/AnimalHervivoro.java
+++ Basico/src/POO/Aprendiendo/Herencia/MetodosAbstactos/AnimalHervivoro.java
@@ -1,6 +1,6 @@
 package POO.Aprendiendo.Herencia.MetodosAbstactos;
 
-public class AnimalHervivioro extends Animal {
+public class AnimalHervivoro extends Animal {
     @Override
     public void alimentarse() {
         System.out.println("El animal hervivoro se alimenta de hierva");
--- Basico/src/POO/Aprendiendo/Herencia/MetodosAbstactos/Principal.java
+++ Basico/src/POO/Aprendiendo/Herencia/MetodosAbstactos/Principal.java
@@ -2,9 +2,14 @@ package POO.Aprendiendo.Herencia.MetodosAbstactos;
 
 public class Principal {
     public static void main(String[] args) {
-        AnimalCarnivoro animal = new AnimalCarnivoro();
+        Planta plant = new Planta();
+        plant.alimentarse();
 
+        AnimalCarnivoro animal = new AnimalCarnivoro();
         animal.alimentarse();
+
+        AnimalHervivoro hervivoro = new AnimalHervivoro();
+        hervivoro.alimentarse();
 
     }
 }
